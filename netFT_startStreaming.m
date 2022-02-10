0de388fde3830100d556ac258c557836a8117441
geunee20@gmail.com <geunee20@gmail.com>
2022-02-10 17:26:03 -0500

    v2 for udpport instead of udp

--- netFT_startStreaming.m
+++ netFT_startStreaming.m
@@ -1,8 +1,10 @@
 function [  ] = netFT_startStreaming( u )
-%send the start streaming command
+% send the start streaming command
 
 startRequest = ['12';'34';'00';'02';'00';'00';'00';'00'];
 startCommand = hexlistToString(startRequest);
-fwrite(u, startCommand) % send message to start rdt streaming
-end
+netFT = "192.168.1.1";
+port = 49152;
+write(u, startCommand, "string", netFT, port) % send message to start rdt streaming
 
+end
